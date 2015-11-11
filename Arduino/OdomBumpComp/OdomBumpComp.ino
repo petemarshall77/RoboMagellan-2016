340dd338b7649839dfe690b84371ea9e11ea9c48
Pete Marshall <petemarshall77@gmail.com>
2015-11-10 18:00:53 -0800

    cleaned up compass code

--- Arduino/OdomBumpComp/OdomBumpComp.ino
+++ Arduino/OdomBumpComp/OdomBumpComp.ino
@@ -1,61 +1,69 @@
 #include <Wire.h>
-int HMC6352Address = 0x42;
-int slaveAddress;
-int ledPin = 13;
-boolean ledState = false;
-byte headingData[2];
-int i, headingValue;
+
+int compass_slave_address = 0x42;     //address of compass controller
+int compass_read_address = 0x41;      //read command for compass
+
 int HallState;
-int bump;
 int HallState_Last = 0;
 int count = 0;
+
+int bump;
+
+const int bump_pin = 4;
+const int hall_effect_pin = 7;
+
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(4, INPUT_PULLUP);
-  digitalWrite(2, HIGH);
-  pinMode(7,INPUT);
+  pinMode(bump_pin, INPUT_PULLUP);
+  pinMode(hall_effect_pin,INPUT);
   Serial.begin(9600);
-  slaveAddress = HMC6352Address >> 1;   
-  pinMode(ledPin, OUTPUT);    
+  compass_slave_address = compass_slave_address >> 1;      //magic!! (weird compass thing)
   Wire.begin();
 }
 
 void loop()
 {
-    HallState = digitalRead(7);
+  
+  /*
+   //========== Rotation Sensor ============
+    HallState = digitalRead(hall_effect_pin);
     if (HallState != HallState_Last) {
         // if the state has changed, increment the counter
         if (HallState == HIGH) {
           // if the current state is HIGH then the button
-          // wend from off to on:
+          // went from off to on:
           count++;
         }
         else {
           // if the current state is LOW then the button
-          // wend from on to off:
+          // went from on to off:
         }
       }
     // save the current state as the last state,
     //for next time through the loop
     HallState_Last = HallState;
-    Wire.beginTransmission(slaveAddress);
-    Wire.write("A");              
+    */
+    
+    //=========== Compass =============
+    Wire.beginTransmission(compass_slave_address);
+    Wire.write(compass_read_address);              
     Wire.endTransmission();
-    delay(10);                  
-    Wire.requestFrom(slaveAddress, 2);      
-    i = 0;
-    if(digitalRead(4) == LOW) bump = 1;
-    if(digitalRead(4) == HIGH) bump = 0;
-    while(Wire.available() && i < 2)
-    { 
-      headingData[i] = Wire.read();
-      i++;
-    }
+    delay(6);            
+    
+    Wire.requestFrom(compass_slave_address, 2);      
+    byte MSB = Wire.read();
+    byte LSB = Wire.read();
+    int headingValue = (MSB << 8) + LSB;
+    headingValue = headingValue / 10;
+    
+    //============ Bump Switch ==============
+    if(digitalRead(bump_pin) == LOW) bump = 1;
+    else bump = 0;
+    
+    //========== Write out data =============
     Serial.print(count);
     Serial.print(",");
-    headingValue = headingData[0]*256 + headingData[1]; 
-    Serial.print(int (headingValue / 10));
+    Serial.print(headingValue);
     Serial.print(",");
     Serial.println(bump);
   }
